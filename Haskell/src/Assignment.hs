0cffa761f324179492fed1d75a94015dd4d125c6
Abhijit Upadhyay <abhijitupadhyay03@gmail.com>
2024-09-27 22:36:57 +1000

    Add parser for bold and strikethrough

--- Haskell/src/Assignment.hs
+++ Haskell/src/Assignment.hs
@@ -1,21 +1,32 @@
-module Assignment (markdownParser, convertADTHTML, italicsParser) where
+module Assignment (markdownParser, convertADTHTML, boldParser, italicsParser, strikethroughParser) where
 
 
 import           Data.Time.Clock  (getCurrentTime)
 import           Data.Time.Format (defaultTimeLocale, formatTime)
 import           Instances        (Parser (..))
-import           Parser           (is, satisfy)
+import           Parser           (is, isNot, string, char)
 import           Control.Applicative
 
 data ADT = Empty 
           | Italics String
+          | Bold String
+          | Strikethrough String
   -- Your ADT **must** derive Show.
   deriving (Show, Eq)
 
 
--- Parser for italics
+-- Parser for italics (_)
 italicsParser :: Parser ADT
-italicsParser = (Italics <$> (is '_' *> some (satisfy (/= '_')) <* is '_')) <|> pure Empty
+italicsParser = (Italics <$> (is '_' *> some (isNot '_') <* is '_')) <|> pure Empty
+
+-- Parser for bold (**)
+boldParser :: Parser ADT
+boldParser = (Bold <$> (string "**" *> some (isNot '*') <* string "**")) <|> pure Empty
+
+-- Parser for strikethrough (~~)
+strikethroughParser :: Parser ADT
+strikethroughParser = (Strikethrough <$> (string "~~" *> some (isNot '~') <* string "~~")) <|> pure Empty
+
 
 markdownParser :: Parser ADT
 markdownParser = pure Empty
@@ -26,3 +37,5 @@ getTime = formatTime defaultTimeLocale "%Y-%m-%dT%H:%M:%S" <$> getCurrentTime
 convertADTHTML :: ADT -> String
 convertADTHTML Empty = ""
 convertADTHTML (Italics s) = "<i>" ++ s ++ "</i>"
+convertADTHTML (Bold s) = "<b>" ++ s ++ "</b>"
+convertADTHTML (Strikethrough s) = "<s>" ++ s ++ "</s>"
