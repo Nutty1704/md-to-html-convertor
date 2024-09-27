e332721e694b73d57a6a9ae9aee678cba5c7cd99
Abhijit Upadhyay <abhijitupadhyay03@gmail.com>
2024-09-27 22:03:57 +1000

    Add parser for italics

--- Haskell/src/Assignment.hs
+++ Haskell/src/Assignment.hs
@@ -1,14 +1,21 @@
-module Assignment (markdownParser, convertADTHTML) where
+module Assignment (markdownParser, convertADTHTML, italicsParser) where
 
 
 import           Data.Time.Clock  (getCurrentTime)
 import           Data.Time.Format (defaultTimeLocale, formatTime)
 import           Instances        (Parser (..))
-import           Parser           ()
+import           Parser           (is, satisfy)
+import           Control.Applicative
 
-data ADT = Empty
+data ADT = Empty 
+          | Italics String
   -- Your ADT **must** derive Show.
   deriving (Show, Eq)
+
+
+-- Parser for italics
+italicsParser :: Parser ADT
+italicsParser = (Italics <$> (is '_' *> some (satisfy (/= '_')) <* is '_')) <|> pure Empty
 
 markdownParser :: Parser ADT
 markdownParser = pure Empty
@@ -17,4 +24,5 @@ getTime :: IO String
 getTime = formatTime defaultTimeLocale "%Y-%m-%dT%H:%M:%S" <$> getCurrentTime
 
 convertADTHTML :: ADT -> String
-convertADTHTML Empty = "IMPLEMENT_THIS"
+convertADTHTML Empty = ""
+convertADTHTML (Italics s) = "<i>" ++ s ++ "</i>"
